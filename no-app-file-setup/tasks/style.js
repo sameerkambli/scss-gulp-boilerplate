cc0d0b2bf3d5828dc750654ec25abbe3358249c1
sameer.kambli <sameer.kambli@sts.in>
2019-07-12 21:41:14 +0530

    No App File - Upgraded task & packages version as per gulp version-4 requirements

--- no-app-file-setup/tasks/style.js
+++ no-app-file-setup/tasks/style.js
@@ -10,14 +10,15 @@ config.sassSrcDir    = './scss';
 config.cssPath       = './css';
 
 // Vendor CSS Function
-gulp.task('vendor-css', function () {
+gulp.task('vendor-css', done => {
     gulp.src(config.sassSrcDir + '/vendor/*.css')
         .pipe(concat('vendor.css'))
         .pipe(gulp.dest(config.cssPath));
+    done();
 });
 
 // Main CSS Function
-gulp.task('main-css', function () {
+gulp.task('main-css', done => {
     var baseStream = gulp.src(config.sassSrcDir + '/base/main.scss')
         .pipe(plumber())
         .pipe(sass({
@@ -26,14 +27,19 @@ gulp.task('main-css', function () {
         }))
         .pipe(concat('main.css'))
         .pipe(gulp.dest(config.cssPath))
+    done();
 });
 
 // Gulp Task Merged Function
-gulp.task('build-scss', function () {
-    gulp.start('vendor-css', 'main-css');
-});
+gulp.task('build-scss', gulp.series(
+    gulp.parallel('vendor-css', 'main-css')
+));
 
-// Gulp Watch Command
-gulp.task('build-scss:watch', function() {
-    gulp.watch('./scss/**/*.*', ['build-scss']);
+gulp.task('build-scss:watch', () => {
+    gulp.watch('./scss/**/*.scss*', (done) => {
+        gulp.series(
+            gulp.parallel('vendor-css', 'main-css')
+        )
+        (done);
+    });
 });
