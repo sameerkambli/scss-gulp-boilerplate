ae3abdce9f0f4f4f7bcb90db854bbbcd40c883e8
sameer.kambli <sameer.kambli@sts.in>
2019-07-12 21:38:37 +0530

    Upgraded task & packages version as per gulp version-4 requirements

--- app-file-setup/tasks/style.js
+++ app-file-setup/tasks/style.js
@@ -2,7 +2,6 @@ var gulp    = require('gulp'),
     sass    = require('gulp-sass'),
     plumber = require('gulp-plumber'),
     concat  = require('gulp-concat'),
-    gutil   = require('gulp-util'),
     _       = require('underscore'),
     config  = module.exports = _.clone(require('../config/config'));
 
@@ -11,14 +10,15 @@ config.sassSrcDir    = './scss';
 config.cssPath       = './css';
 
 // Vendor CSS Function
-gulp.task('vendor-css', function () {
+gulp.task('vendor-css', done => {
     gulp.src(config.sassSrcDir + '/vendor/*.css')
         .pipe(concat('vendor.css'))
         .pipe(gulp.dest(config.cssPath));
+    done();
 });
 
 // Main CSS Function
-gulp.task('main-css', function () {
+gulp.task('main-css', done => {
     var baseStream = gulp.src(config.sassSrcDir + '/base/main.scss')
         .pipe(plumber())
         .pipe(sass({
@@ -27,10 +27,11 @@ gulp.task('main-css', function () {
         }))
         .pipe(concat('main.css'))
         .pipe(gulp.dest(config.cssPath))
+    done();
 });
 
 // App CSS Function
-gulp.task('app-css', function () {
+gulp.task('app-css', done => {
     gulp.src(config.sassSrcDir + '/application/' + config.app + '.scss')
         .pipe(plumber())
         .pipe(sass({
@@ -41,14 +42,19 @@ gulp.task('app-css', function () {
             ]
         }))
         .pipe(gulp.dest(config.cssPath))
+    done();
 });
 
 // Gulp Task Merged Function
-gulp.task('build-scss', function () {
-    gulp.start('vendor-css', 'main-css', 'app-css');
-});
+gulp.task('build-scss', gulp.series(
+    gulp.parallel('vendor-css', 'main-css', 'app-css')
+));
 
-// Gulp Watch Command
-gulp.task('build-scss:watch', function() {
-    gulp.watch('./scss/**/*.*', ['build-scss']);
+gulp.task('build-scss:watch', () => {
+    gulp.watch('./scss/**/*.scss*', (done) => {
+        gulp.series(
+            gulp.parallel('vendor-css', 'main-css', 'app-css')
+        )
+        (done);
+    });
 });
