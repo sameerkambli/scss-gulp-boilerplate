61aac8caba5e558f152cca4136a258af19b0f90c
Sameer.Kambli <sameer.kambli@dreambridge.global>
2023-05-03 12:21:26 +0530

    modified gulp task

--- app-file-setup/gulpfile.js
+++ app-file-setup/gulpfile.js
@@ -1,2 +1,57 @@
-// Load in all gulp tasks from subdirectory
-require('require-dir')('tasks');
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass')(require('sass')),
+    concat  = require('gulp-concat'),
+    _       = require('underscore'),
+    config  = module.exports = _.clone(require('../config/config'));
+
+// SCSS & CSS Path
+config.sassSrcDir    = './scss';
+config.cssPath       = './css';
+
+// Vendor CSS Function
+gulp.task('vendor-css', done => {
+    gulp.src(config.sassSrcDir + '/vendor/*.css')
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest(config.cssPath));
+    done();
+});
+
+// Main CSS Function
+gulp.task('main-css', done => {
+    var baseStream = gulp.src(config.sassSrcDir + '/base/main.scss')
+        .pipe(sass({
+            sourceComments: 'map',
+            outputStyle: 'expanded' // expanded or compressed
+        }))
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest(config.cssPath))
+    done();
+});
+
+// App CSS Function
+gulp.task('app-css', done => {
+    gulp.src(config.sassSrcDir + '/application/' + config.app + '.scss')
+        .pipe(sass({
+            sourceComments: 'map',
+            outputStyle: 'expanded', // expanded or compressed
+            includePaths: [
+                require('node-bourbon').includePaths
+            ]
+        }))
+        .pipe(gulp.dest(config.cssPath))
+    done();
+});
+
+// Gulp Task Merged Function
+gulp.task('build-scss', gulp.series(
+    gulp.parallel('vendor-css', 'main-css', 'app-css')
+));
+
+gulp.task('build-scss:watch', () => {
+    gulp.watch('./scss/**/*.scss*', (done) => {
+        gulp.series(
+            gulp.parallel('vendor-css', 'main-css', 'app-css')
+        )
+        (done);
+    });
+});
